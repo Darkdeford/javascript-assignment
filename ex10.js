fb0c35ef5585dacefbc2f89c181aa90416b7c7e8
Bartłomiej Łoza <bloza@pretius.com>
2016-09-27 07:47:25 +0200

    task10

--- ex10.js
+++ ex10.js
@@ -30,3 +30,29 @@ Create a spy that keeps track of how many times a function is called.
   * Functions have context, input and output. Make sure you consider the context, input to *and output from* the function you are spying on.
 
 */
+
+function Spy(object, method) {
+    let self = {
+        count: 0
+    };
+
+    let basicMethod = object[method];
+    object[method] = (...args) => {
+        self.count++;
+        basicMethod.apply(object, args);
+    };
+    return self;
+}
+
+let spy = Spy(console, 'error');
+console.log(spy.count);
+// 0
+
+console.error('Error occured');
+console.log(spy.count);
+//1
+
+console.error('Error occured');
+console.error('Error occured');
+console.log(spy.count);
+//3
