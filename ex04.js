079431b025325267bc1f835cb56939b791dbd07c
Bartłomiej Łoza <bloza@pretius.com>
2016-09-27 07:06:43 +0200

    task4

--- ex04.js
+++ ex04.js
@@ -4,9 +4,18 @@
 // * Do not create any unnecessary functions e.g. helpers.
 
 function doubleAll(numbers) {
-  var result = []
+  var result = [];
   for (var i = 0; i < numbers.length; i++) {
     result.push(numbers[i] * 2)
   }
   return result
 }
+
+function doubleAll2(numbers) {
+  return numbers.map(number => number * 2);
+}
+
+//TEST
+const numbers = [1,3,5,7,9,11];
+console.log('doubleAll:  ', doubleAll(numbers));
+console.log('doubleAll2: ', doubleAll2(numbers));
